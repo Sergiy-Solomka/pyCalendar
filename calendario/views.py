52aa69b42e43d1a4c4d16da9a3c6ceeb6ee8eb08
Sergiy Solomka <ssolomka@gmail.com>
2016-07-07 14:32:13 +0200

    pequeños cambio enombres..reparado el month.html

--- calendario/views.py
+++ calendario/views.py
@@ -24,5 +24,5 @@ def get_month_events(request):
         return HttpResponse(json.dumps(response))
 
 
-def day_event(request):
+def get_day_event(request):
     return render(request, 'calendario/day.html')
