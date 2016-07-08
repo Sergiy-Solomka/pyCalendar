73d3420b38463ab7c51fe9fe24c192316f1bd3d2
Sergiy Solomka <ssolomka@gmail.com>
2016-07-08 16:18:54 +0200

    utilisando class booking,para index html

--- calendario/urls.py
+++ calendario/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'calendario'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^getmonthevents', views.get_month_events, name='monthevents'),
+    url(r'^getmonthevents', views.get_month_bookings, name='monthevents'),
     url(r'^getdayevents', views.get_day_events, name='dayevents'),
 ]
--- calendario/views.py
+++ calendario/views.py
@@ -2,19 +2,19 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db.models import Count
-from .models import Event
+from .models import Event,Booking
 
 
 def index(request):
     return render(request, 'calendario/month.html')
 
 
-def get_month_events(request):
+def get_month_bookings(request):
     if request.is_ajax():
         month = request.GET['month']
         year = request.GET['year']
 
-        result = Event.objects.filter(date__year=year, date__month=month).values('date').annotate(
+        result = Booking.objects.filter(date__year=year, date__month=month).values('date').annotate(
             number_of_events=Count('date'))
 
         response = {}
