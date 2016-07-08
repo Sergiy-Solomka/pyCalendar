367d3012aae613fbb09219f739728a8dcf857cb1
Sergiy Solomka <ssolomka@gmail.com>
2016-07-08 16:09:46 +0200

    Actualizado,modelo booking

--- calendario/models.py
+++ calendario/models.py
@@ -20,9 +20,9 @@ class Booking(models.Model):
     date = models.DateField()
     time = models.TimeField(default=0)
     pax = models.IntegerField(default=0)
-    name = models.CharField(default='',max_length=10)
+    name = models.CharField(default='',max_length=20)
     table = models.IntegerField(default=0)
-    comments = models.TextField()
+    comments = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return str(self.date) + ' -- ' + str(self.time) + ' -- Pax: ' + str(self.pax) + ' -- Name: ' + self.name + ' -- Table: ' + str(self.table) + ' -- (' + self.comments + ')'
