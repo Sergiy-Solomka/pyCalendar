fa1ff205fb312acd2ca86e476aceaa399e65702b
Sergiy Solomka <ssolomka@gmail.com>
2016-07-15 16:22:00 +0200

    funciona opcion de eligir hora preformada en reserva

--- calendario/models.py
+++ calendario/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
 
-
 class Booking(models.Model):
+    TIME_CHOICES = (
+        ('18:00:00', '18:00:00'),
+        ('6:15', '6:15'),
+        ('6:30', '6:30'),
+        ('6:45', '6:45'),
+        ('7:00', '7:00'),
+        ('7:15', '7:15'),
+        ('7:30', '7:30'),
+        ('7:45', '7:45'),
+        ('8:00', '8:00'),
+        ('8:15', '8:15'),
+        ('8:30', '8:30'),
+        ('8:45', '8:45'),
+        ('9:00', '9:00'),
+        ('9:15', '9:15'),
+        ('9:30', '9:30'),
+        ('9:45', '9:45'),
+        ('10:00', '10:00'),
+    )
     date = models.DateField()
-    time = models.TimeField(default=0)
+    time = models.CharField(max_length =4,choices=TIME_CHOICES)
     pax = models.IntegerField(default=0)
     name = models.CharField(default='',max_length=20)
     table = models.IntegerField(default=0)
