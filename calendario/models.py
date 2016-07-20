bd6b9cefbe8f2402b280f15ee30f8e8ecc040a72
Sergiy Solomka <ssolomka@gmail.com>
2016-07-20 09:49:47 +0200

    añadido numero de mesa fijo en modelo

--- calendario/models.py
+++ calendario/models.py
@@ -21,11 +21,40 @@ class Booking(models.Model):
         ('21:45', '21:45'),
         ('22:00', '22:00')
         )
+
+    TABLE_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10'),
+        ('12', '12'),
+        ('14', '14'),
+        ('15', '15'),
+        ('16', '16'),
+        ('17', '17'),
+        ('18', '18'),
+        ('19', '19'),
+        ('20', '20'),
+        ('21', '21'),
+        ('22', '22'),
+        ('23', '23'),
+        ('30', '30'),
+        ('31', '31'),
+        ('32', '32'),
+        ('33', '33'),
+        ('34', '34'),
+        ('35', '35'),
+    )
     date = models.DateField()
     time = models.CharField(max_length =5,choices=TIME_CHOICES)
     pax = models.IntegerField(default=0)
     name = models.CharField(default='',max_length=20)
-    table = models.IntegerField(default=0)
+    table = models.CharField(max_length =2,choices=TABLE_CHOICES)
     comments = models.TextField(blank=True,null=True)
 
     def __str__(self):
