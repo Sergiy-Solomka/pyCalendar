78cdbb212b31d89136074e6eb2f9cd42460826e0
AbixSol <ssolomka@gmail.com>
2016-07-27 15:20:10 +0200

    pq cambios en modelo

--- calendario/models.py
+++ calendario/models.py
@@ -2,6 +2,37 @@ from django.db import models
 
 
 class Booking(models.Model):
+    TABLE_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('105', '105'),
+        ('6', '6'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10'),
+        ('12', '12'),
+        ('14', '14'),
+        ('15', '15'),
+        ('115', '115'),
+        ('16', '16'),
+        ('116', '116'),
+        ('17', '17'),
+        ('18', '18'),
+        ('19', '19'),
+        ('20', '20'),
+        ('21', '21'),
+        ('22', '22'),
+        ('23', '23'),
+        ('30', '30'),
+        ('31', '31'),
+        ('32', '32'),
+        ('33', '33'),
+        ('34', '34'),
+        ('35', '35'),
+    )
     TIME_CHOICES = (
         ('18:00', '18:00'),
         ('18:15', '18:15'),
@@ -22,39 +53,11 @@ class Booking(models.Model):
         ('22:00', '22:00')
         )
 
-    TABLE_CHOICES = (
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-        ('6', '6'),
-        ('8', '8'),
-        ('9', '9'),
-        ('10', '10'),
-        ('12', '12'),
-        ('14', '14'),
-        ('15', '15'),
-        ('16', '16'),
-        ('17', '17'),
-        ('18', '18'),
-        ('19', '19'),
-        ('20', '20'),
-        ('21', '21'),
-        ('22', '22'),
-        ('23', '23'),
-        ('30', '30'),
-        ('31', '31'),
-        ('32', '32'),
-        ('33', '33'),
-        ('34', '34'),
-        ('35', '35'),
-    )
     date = models.DateField()
     time = models.CharField(max_length =5,choices=TIME_CHOICES)
     pax = models.IntegerField(default=0)
     name = models.CharField(default='',max_length=20)
-    table = models.CharField(max_length =2,choices=TABLE_CHOICES)
+    table = models.CharField(max_length =3,choices=TABLE_CHOICES)
     comments = models.TextField(blank=True,null=True)
 
     def __str__(self):
