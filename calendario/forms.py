01bbe46c64597c5eb34e4deff313fa77d698d2a9
AbixSol <ssolomka@gmail.com>
2016-08-03 00:01:47 +0200

    add field phone in model. but its not showing in day bookings,and new booking adding now in new window

--- calendario/forms.py
+++ calendario/forms.py
@@ -6,4 +6,4 @@ from calendario.models import Booking
 class PostForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ('date', 'time', 'pax', 'name', 'tables', 'comments')
+        fields = ('date', 'time', 'pax', 'name', 'tables', 'comments','phone')
